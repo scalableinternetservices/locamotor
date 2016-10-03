c22997ae7f6af02a478d951b2abf9f5f4296c387
Aaron Magat <aaronmgt@gmail.com>
2016-10-03 00:02:08 -0700

    Basic controller to get things started

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  root 'main#home'
+
+  get 'main/home'
+  get 'main/help'
+  get 'main/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  #root 'application#home'
 end
