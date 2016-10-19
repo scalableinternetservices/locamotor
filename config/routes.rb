f5174916480b724ec0606c71f3ec8a209c7c3687
aaronzak <aaronzakhor@gmail.com>
2016-10-18 20:51:38 -0700

    fixed bugs and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
 
   root 'main#home'
 
+  get 'users/new'
+  get 'posts/new'
+  get  '/signup',  to: 'users#new'
   get 'main/home'
   get 'main/help'
   get 'main/about'
