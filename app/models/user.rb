64a9a7b42b5a4675cebdd9003a588477662003f3
Aaron Magat <aaronmgt@gmail.com>
2016-10-19 20:58:21 -0700

    adding users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :created_posts, :foreign_key => "creator_id", :class_name => "Post"
   has_many :claimed_posts, :foreign_key => "claimer_id", :class_name => "Post"
 
   before_save { self.email = email.downcase }
-  validates :name, presence: true, length: { maximum: 50 }
+  # validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
                     format: { with: VALID_EMAIL_REGEX },
