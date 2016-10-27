4429733d26b83b1957bbf7167528a735c1ca4e6d
joelddick <joel.d.dick@gmail.com>
2016-10-26 18:11:41 -0700

    Saving is hard

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,10 +1,10 @@
-class Message < ApplicationRecord
-  belongs_to :conversation
-  belongs_to :user
+# class Message < ApplicationRecord
+#   belongs_to :conversation
+#   belongs_to :user
 
-  validates_presence_of :content, :conversation_id, :user_id
+#   validates_presence_of :content, :conversation_id, :user_id
 
-  def message_time
-  	created_at.strftime("%v")
-  end
-end
+#   def message_time
+#   	created_at.strftime("%v")
+#   end
+# end
