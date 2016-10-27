14a4961488793ea10671c27572bbbeda89d0f2ca
joelddick <joel.d.dick@gmail.com>
2016-10-26 17:58:21 -0700

    Messages are bull

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -11,7 +11,7 @@ class Conversation < ApplicationRecord
 	end
 
 	scope :between, -> (sender_id, recipient_id) do
-		where ("(conversations.sender_id = ? AND conversations.recipient_id = ?) OR (conversations.sender_id = ? AND conversations.recipient_id = ?)",
-			sender_id, recipient_id, recipient_id, sender_id)
+		#where ("conversations.sender_id = ? AND conversations.recipient_id = ? OR conversations.sender_id = ? AND conversations.recipient_id = ?", sender_id, recipient_id, recipient_id, sender_id)
+		where ("conversations.sender_id = ? AND conversations.recipient_id = ?", sender_id, recipient_id)
 	end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable#, :confirmable
   has_many :created_posts, :foreign_key => "creator_id", :class_name => "Post"
   has_many :claimed_posts, :foreign_key => "claimer_id", :class_name => "Post"
 
