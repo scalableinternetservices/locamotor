0c557e1abe79790ac3007af6c695047e850e51b4
Joel <joel.d.dick@gmail.com>
2016-11-09 15:13:32 -0800

    Seed file added

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+location = Location.create! :address =>'Goleta'
+
+for i in 1..100
+	user = User.create! :email => 'user' + i.to_s + '@gmail.com', :password => '1234567', :password_confirmation => '1234567'
+	post = Post.create! :email => 'user' + i.to_s + '@gmail.com', :vehicle => 'car', :start_time => DateTime.parse('01/01/2016, 12:00PM'), :end_time => DateTime.parse('01/01/2016, 12:00PM'), :price => '10', :max_radius => '25', :auto_book => 'auto_book', :start_location_id => location.id, :post_type => 'FR', :creator_id => user.id
+end
