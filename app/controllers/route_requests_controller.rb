3dd1ad2ae095330caf8e8b325b24a86939df9fa3
Shane Masuda <masuda@umail.ucsb.edu>
2016-11-09 01:20:36 +0000

    Added the route_requests functionality, some bugs in the code

--- app/controllers/route_requests_controller.rb
+++ app/controllers/route_requests_controller.rb
@@ -1,5 +1,54 @@
 class RouteRequestsController < ApplicationController
 
+  @completedRoutes = Array.new
+  
+  @allPosts = Array.new
+  Post.all.each do |post|
+    @allPosts << post
+  end
+  
+  def newLocation(startLocation, endLocation, distance)
+    if distance/startLocation.distance_to(endLocation) < 1
+      @ratio = distance/startLocation.distance_to(endLocation)
+    else
+      @ratio = 1
+    end
+   @finalLocation = Location.new
+   @finalLocation.latitude = (endLocation.latitude - startLocation.latitude) * @ratio + startLocation.latitude
+   @finalLocation.longitude = (endLocation.longitude - startLocation.longitude) * @ratio + startLocation.longitude
+   return @finalLocation
+  end
+  
+  def getNearbyPosts(startLocation)
+    @nearbyPosts = Array.new
+    @allPosts.all.each do |post|
+      if Location.find(post.start_location_id).distance_to(startLocation) < post.max_radius
+        @nearbyPosts << post
+      end
+    end
+    return @nearbyPosts
+  end
+  
+  def appendRoute(singleRoute, startLocation, endLocation)
+    if startLocation.distance_to(endLocation) < 0.01
+      @completedRoutes << singleRoute
+      return
+    end
+    getNearbyPosts(startLocation).each do |post|
+      @newSingleRoute = singleRoute.dup
+      @newSingleRoute << post
+      appendRoute(@newSingleRoute, newLocation(startLocation, endLocation, post.max_radius - Location.find(post.start_location_id).distance_to(startLocation)), endLocation)
+    end
+  end
+  
+  def netCost(postArray) 
+    @cost = 0
+    postArray.each do |post|
+      @cost += post.price
+    end
+    return @cost
+  end
+  
   def create
   	routes_args = params[:routes]
 
@@ -17,8 +66,9 @@ class RouteRequestsController < ApplicationController
     if @route_request.valid?
       @route_request.save
     end
-
-    redirect_to "/route_requests/index"
+    
+    appendRoute(Array.new, @start_Location, @end_Location) 
+    @completedRoutes.sort_by{|route| netCost(route)}
   end
 
   def new
