36fc1f751d56c6e2f82571443c276bd2a4215174
Joel <joel.d.dick@gmail.com>
2016-11-17 17:43:28 -0800

    More seed update.

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,8 +13,71 @@ location = Location.create! :address =>'Goleta'
 end
 
 inserts = []
-10000.times do
-  inserts.push "('user1@gmail.com', 'car', '2016-11-17 17:00:00', '2016-11-17 17:00:00', 'skeddit', 5, '2016-01-02 01:00:00', '2016-06-05 01:00:00', 1, 'auto_book_true', 100, 1)"
+1000.times do
+  inserts.push "('user1@gmail.com', 'Car', '2016-11-17 17:00:00', '2016-11-17 17:00:00', 'skeddit', 1, '2016-01-02 01:00:00', '2016-06-05 01:00:00', 1, 'auto_book_true', 100, 1)"
+end
+sql = "INSERT INTO posts (email, vehicle, created_at, updated_at, description, price, start_time, end_time, creator_id, auto_book, max_radius, start_location_id) VALUES #{inserts.join(", ")}"
+ActiveRecord::Base.connection.insert_sql(sql)
+
+inserts = []
+1000.times do
+  inserts.push "('user1@gmail.com', 'Car', '2016-11-17 17:00:00', '2016-11-17 17:00:00', 'skeddit', 2, '2016-01-02 01:00:00', '2016-06-05 01:00:00', 1, 'auto_book_true', 100, 1)"
+end
+sql = "INSERT INTO posts (email, vehicle, created_at, updated_at, description, price, start_time, end_time, creator_id, auto_book, max_radius, start_location_id) VALUES #{inserts.join(", ")}"
+ActiveRecord::Base.connection.insert_sql(sql)
+
+inserts = []
+1000.times do
+  inserts.push "('user1@gmail.com', 'Car', '2016-11-17 17:00:00', '2016-11-17 17:00:00', 'skeddit', 3, '2016-01-02 01:00:00', '2016-06-05 01:00:00', 1, 'auto_book_true', 100, 1)"
+end
+sql = "INSERT INTO posts (email, vehicle, created_at, updated_at, description, price, start_time, end_time, creator_id, auto_book, max_radius, start_location_id) VALUES #{inserts.join(", ")}"
+ActiveRecord::Base.connection.insert_sql(sql)
+
+inserts = []
+1000.times do
+  inserts.push "('user1@gmail.com', 'Car', '2016-11-17 17:00:00', '2016-11-17 17:00:00', 'skeddit', 4, '2016-01-02 01:00:00', '2016-06-05 01:00:00', 1, 'auto_book_true', 100, 1)"
+end
+sql = "INSERT INTO posts (email, vehicle, created_at, updated_at, description, price, start_time, end_time, creator_id, auto_book, max_radius, start_location_id) VALUES #{inserts.join(", ")}"
+ActiveRecord::Base.connection.insert_sql(sql)
+
+inserts = []
+1000.times do
+  inserts.push "('user1@gmail.com', 'Car', '2016-11-17 17:00:00', '2016-11-17 17:00:00', 'skeddit', 5, '2016-01-02 01:00:00', '2016-06-05 01:00:00', 1, 'auto_book_true', 100, 1)"
+end
+sql = "INSERT INTO posts (email, vehicle, created_at, updated_at, description, price, start_time, end_time, creator_id, auto_book, max_radius, start_location_id) VALUES #{inserts.join(", ")}"
+ActiveRecord::Base.connection.insert_sql(sql)
+
+inserts = []
+1000.times do
+  inserts.push "('user1@gmail.com', 'Car', '2016-11-17 17:00:00', '2016-11-17 17:00:00', 'skeddit', 6, '2016-01-02 01:00:00', '2016-06-05 01:00:00', 1, 'auto_book_true', 100, 1)"
+end
+sql = "INSERT INTO posts (email, vehicle, created_at, updated_at, description, price, start_time, end_time, creator_id, auto_book, max_radius, start_location_id) VALUES #{inserts.join(", ")}"
+ActiveRecord::Base.connection.insert_sql(sql)
+
+inserts = []
+1000.times do
+  inserts.push "('user1@gmail.com', 'Car', '2016-11-17 17:00:00', '2016-11-17 17:00:00', 'skeddit', 7, '2016-01-02 01:00:00', '2016-06-05 01:00:00', 1, 'auto_book_true', 100, 1)"
+end
+sql = "INSERT INTO posts (email, vehicle, created_at, updated_at, description, price, start_time, end_time, creator_id, auto_book, max_radius, start_location_id) VALUES #{inserts.join(", ")}"
+ActiveRecord::Base.connection.insert_sql(sql)
+
+inserts = []
+1000.times do
+  inserts.push "('user1@gmail.com', 'Car', '2016-11-17 17:00:00', '2016-11-17 17:00:00', 'skeddit', 8, '2016-01-02 01:00:00', '2016-06-05 01:00:00', 1, 'auto_book_true', 100, 1)"
+end
+sql = "INSERT INTO posts (email, vehicle, created_at, updated_at, description, price, start_time, end_time, creator_id, auto_book, max_radius, start_location_id) VALUES #{inserts.join(", ")}"
+ActiveRecord::Base.connection.insert_sql(sql)
+
+inserts = []
+1000.times do
+  inserts.push "('user1@gmail.com', 'Car', '2016-11-17 17:00:00', '2016-11-17 17:00:00', 'skeddit', 9, '2016-01-02 01:00:00', '2016-06-05 01:00:00', 1, 'auto_book_true', 100, 1)"
+end
+sql = "INSERT INTO posts (email, vehicle, created_at, updated_at, description, price, start_time, end_time, creator_id, auto_book, max_radius, start_location_id) VALUES #{inserts.join(", ")}"
+ActiveRecord::Base.connection.insert_sql(sql)
+
+inserts = []
+1000.times do
+  inserts.push "('user1@gmail.com', 'Boat', '2016-11-17 17:00:00', '2016-11-17 17:00:00', 'skeddit', 10, '2016-01-02 01:00:00', '2016-06-05 01:00:00', 1, 'auto_book_true', 100, 1)"
 end
 sql = "INSERT INTO posts (email, vehicle, created_at, updated_at, description, price, start_time, end_time, creator_id, auto_book, max_radius, start_location_id) VALUES #{inserts.join(", ")}"
 ActiveRecord::Base.connection.insert_sql(sql)
