d77f93656116de55d470ed5165f4e61714291f4d
Joel <joel.d.dick@gmail.com>
2016-11-17 16:35:02 -0800

    More seed update.

--- db/seeds.rb
+++ db/seeds.rb
@@ -16,5 +16,5 @@ inserts = []
 10000.times do
   inserts.push "(user1@gmail.com, car, '2016-11-17 17:00:00', '2016-11-17 17:00:00', skeddit, 5, '2016-01-02 01:00:00', '2016-06-05 01:00:00', 1, auto_book_true, 100, 1)"
 end
-sql = "INSERT INTO posts ('email', 'vehicle', 'created_at', 'updated_at', 'description', 'price', 'start_time', 'end_time', 'creator_id', 'auto_book', 'max_radius', 'start_location_id') VALUES #{inserts.join(", ")}"
+sql = "INSERT INTO posts (email, vehicle, created_at, updated_at, description, price, start_time, end_time, creator_id, auto_book, max_radius, start_location_id) VALUES #{inserts.join(", ")}"
 ActiveRecord::Base.connection.insert_sql(sql)
