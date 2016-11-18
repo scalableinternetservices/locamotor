6d779abad62afa23347a1b14d7bb4cc647448324
Joel <joel.d.dick@gmail.com>
2016-11-17 16:05:38 -0800

    Update seed file with mass insert.

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,14 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+location = Location.create! :address =>'Goleta'
+
 100.times do |count|
   User.create(email: "user#{count+1}@gmail.com", password: "1234567", password_confirmation: "1234567")
 end
+
+inserts = []
+10000.times do
+  inserts.push "(user1@gmail.com, car, '2016-11-17 17:00:00', '2016-11-17 17:00:00', skeddit, 5, '2016-01-02 01:00:00', '2016-06-05 01:00:00', 1, auto_book_true, 100, 1)"
+end
+sql = "INSERT INTO posts (`email`, `vehicle`, `created_at`, `updated_at`, `description`, `price`, `start_time`, `end_time`, `creator_id`, `auto_book`, `max_radius`, `start_location_id`) VALUES #{inserts.join(", ")}"
