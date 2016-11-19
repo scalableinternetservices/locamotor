cfc2f75c1cd1d7e6f40994119ec900ef416863c7
Aaron Magat <aaronmgt@gmail.com>
2016-11-18 17:58:11 -0800

    adding pagination

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,12 +10,17 @@ class UsersController < ApplicationController
     # Create rating that current user can use to rate the shown user
     @rating = current_user.ratings_of.build()
 
-    @can_rate = false
 
+    # Find out if current user has already rated the user who we will be showing
+    @can_rate = false
     if Rating.rating_by_of(current_user.id, params[:id]).present? or current_user.id.to_i == params[:id].to_i
         @can_rate = true
     end
-    # Find out if current user has already rated the user who we will be showing
+
+    # Note that when you go to the next page on any one of these, it brings you to the next page for all of them. Could fix
+    @request_set = Post.where("creator_id = #{params[:id]} and post_type = 'RR'").paginate(page: params[:page], per_page: 3)
+    @offer_set = Post.where("creator_id = #{params[:id]} and post_type = 'FR'").paginate(page: params[:page], per_page: 3)
+    @reservation_set = Reservation.where(user_id: params[:id]).paginate(page: params[:page], per_page: 3)
   end
 
   def new
