ee80458d4dd6b874e5e78609ceeaa28d69abd806
Aaron Magat <aaronmgt@gmail.com>
2016-11-18 18:01:08 -0800

    making user page pagination smarter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,10 +17,9 @@ class UsersController < ApplicationController
         @can_rate = true
     end
 
-    # Note that when you go to the next page on any one of these, it brings you to the next page for all of them. Could fix
-    @request_set = Post.where("creator_id = #{params[:id]} and post_type = 'RR'").paginate(page: params[:page], per_page: 3)
-    @offer_set = Post.where("creator_id = #{params[:id]} and post_type = 'FR'").paginate(page: params[:page], per_page: 3)
-    @reservation_set = Reservation.where(user_id: params[:id]).paginate(page: params[:page], per_page: 3)
+    @request_set = Post.where("creator_id = #{params[:id]} and post_type = 'RR'").paginate(page: params[:request_page], per_page: 3)
+    @offer_set = Post.where("creator_id = #{params[:id]} and post_type = 'FR'").paginate(page: params[:offer_page], per_page: 3)
+    @reservation_set = Reservation.where(user_id: params[:id]).paginate(page: params[:reservation_page], per_page: 3)
   end
 
   def new
