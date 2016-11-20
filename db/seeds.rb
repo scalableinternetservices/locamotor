df4a74b9cf907c2617b7f3acc20f543a94fffeff
Joel <joel.d.dick@gmail.com>
2016-11-20 14:09:57 -0800

    Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -75,7 +75,7 @@ sql = "INSERT INTO posts (email, vehicle, created_at, updated_at, description, p
 ActiveRecord::Base.connection.insert_sql(sql)
 
 inserts = []
-4000.times do
+10000.times do
   inserts.push "('user1@gmail.com', 'Boat', '2016-11-17 17:00:00', '2016-11-17 17:00:00', 'skeddit', 10, '2016-01-02 01:00:00', '2016-06-05 01:00:00', 1, 'auto_book_true', 100, 1)"
 end
 sql = "INSERT INTO posts (email, vehicle, created_at, updated_at, description, price, start_time, end_time, creator_id, auto_book, max_radius, start_location_id) VALUES #{inserts.join(", ")}"
