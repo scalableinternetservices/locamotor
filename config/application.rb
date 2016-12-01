83ebbf1e8d0bf148afa6c79f453e42066cbdd15e
Aaron Magat <aaronmgt@gmail.com>
2016-12-01 01:24:42 -0800

    sucker punch?

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module Locomotor
   class Application < Rails::Application
+    config.active_job.queue_adapter = :sucker_punch
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
